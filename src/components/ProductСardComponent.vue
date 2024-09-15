a9822ba227bedb073ca062c5ae671f2d02b5383a
resAsus <respectedbro@yandex.ru>
2024-09-15 21:08:52 +0300

    added component ProdCardComp

---
+++ src/components/ProductСardComponent.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="best__item">
+    <img src="@/assets/img/coffee-1.jpg" alt="coffee">
+    <div class="best__item-title">
+      Solimo Coffee Beans 2kg
+    </div>
+    <div class="best__item-price">10.73$</div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
