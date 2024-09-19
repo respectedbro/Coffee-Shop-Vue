61ac9c4d4a9e0de04055cebf0872167db25ce110
resAsus <respectedbro@yandex.ru>
2024-09-19 12:35:08 +0300

    navItem props

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,20 +5,27 @@
       <div class="row">
         <div class="col-lg-6 offset-lg-3">
           <ul class="footer d-flex flex-wrap">
-            <li class=" footer__item">
+            <li class="footer__item">
               <router-link :to="links[0].link">
-                <img :src="require(`@/assets/logo/${links[0].icon}`)" :alt="links[0].icon" />
+                <img :src="require(`@/assets/logo/${links[0].icon}`)" :alt="links[0].icon"/>
               </router-link>
             </li>
-            <li class="footer__item">
-              <router-link :to="links[1].link">{{links[1].text}}</router-link>
-            </li>
-            <li class="footer__item">
-              <router-link :to="links[2].link">{{links[2].text}}</router-link>
-            </li>
-            <li class="footer__item">
-              <router-link :to="links[3].link">{{links[3].text}}</router-link>
-            </li>
+            <nav-item
+                classLink="footer__item"
+                :link="links[1].link"
+                :text="links[1].text"
+            />
+
+            <nav-item
+                classLink="footer__item"
+                :link="links[2].link"
+                :text="links[2].text"
+            />
+            <nav-item
+                classLink="footer__item"
+                :link="links[3].link"
+                :text="links[3].text"
+            />
           </ul>
         </div>
       </div>
@@ -28,7 +35,10 @@
 </template>
 
 <script>
+import NavItem from "@/components/NavItem.vue";
+
 export default {
+  components: {NavItem},
   data() {
     return {
       links: [
--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -3,24 +3,34 @@
     <ul class="header d-flex justify-content-center justify-content-md-start flex-wrap">
       <li class="header__item">
         <router-link :to="links[0].link">
-          <img :src="require(`@/assets/logo/${links[0].icon}`)" :alt="links[0].icon" />
+          <img :src="require(`@/assets/logo/${links[0].icon}`)" :alt="links[0].icon"/>
         </router-link>
       </li>
-      <li class="header__item">
-        <router-link :to="links[1].link">{{links[1].text}}</router-link>
-      </li>
-      <li class="header__item">
-        <router-link :to="links[2].link">{{links[2].text}}</router-link>
-      </li>
-      <li class="header__item">
-        <router-link :to="links[3].link">{{links[3].text}}</router-link>
-      </li>
+      <nav-item
+          classLink="header__item"
+          :link="links[1].link"
+          :text="links[1].text"
+      />
+
+      <nav-item
+          classLink="header__item"
+          :link="links[2].link"
+          :text="links[2].text"
+      />
+      <nav-item
+          classLink="header__item"
+          :link="links[3].link"
+          :text="links[3].text"
+      />
     </ul>
   </header>
 </template>
 
 <script>
+import NavItem from "@/components/NavItem.vue";
+
 export default {
+  components: {NavItem},
   data() {
     return {
       links: [
