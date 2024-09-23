bfbccbf1471f77928c6865ec15fd753632992f6e
resAsus <respectedbro@yandex.ru>
2024-09-23 22:54:47 +0300

    din pages

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import giHeroView from "@/views/HeroView.vue";
+import HeroView from "@/views/HeroView.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
